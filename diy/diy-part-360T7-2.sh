4f74adce78cc3cd4f46e2cc088f08e1d410b1152
Jiang <56918767+hkk666@users.noreply.github.com>
2022-11-20 22:23:58 +0800

    Update diy-part-360T7-2.sh

--- diy/diy-part-360T7-2.sh
+++ diy/diy-part-360T7-2.sh
@@ -12,17 +12,14 @@
 #修改默认IP
 sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
-# 移除重复软件包
-rm -rf feeds/luci/themes/luci-theme-argon
-
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon-new/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-new/g' feeds/luci/collections/luci/Makefile
 
 # 修改主机名字，把MSG1500修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/ImmortalWrt/360T7/g' package/base-files/files/bin/config_generate
 
 # Themes
-#git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 -b 18.06 https://github.com/padavanonly/immortalwrtARM/tree/mt7981/package/luci-theme-argon-new package/luci-theme-argon-new
 #git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
